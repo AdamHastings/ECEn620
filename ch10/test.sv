8dbf77fd740c36dd4929c7c9b3f4bdfc5c207071
AdamHastings <itsadamhastings@gmail.com>
2017-12-08 19:36:24 -0700

    fixed test

--- ch10/test.sv
+++ ch10/test.sv
@@ -2,7 +2,7 @@ program automatic test();
 
 	parameter ADDRESS_WIDTH = 8;
 	import env_pkg::*;
-	//import CovPort_pkg::*;
+	import CovPort_pkg::*;
 	Environment #(ADDRESS_WIDTH) env;
 	Driver_cbs_cov #(ADDRESS_WIDTH) dcc;
 	Transaction #(ADDRESS_WIDTH) drv_tr;
